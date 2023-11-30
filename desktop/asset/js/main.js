294a8caf16157374c89c71778a986dc816b989ea
yudantoardi <yudantoardi@gmail.com>
2023-11-30 15:31:05 +0700

    updates

--- desktop/asset/js/main.js
+++ desktop/asset/js/main.js
@@ -11,4 +11,21 @@ $(document).ready(function(){
         }
     });
 
+    $(window).scroll(function(){
+        if ($(window).scrollTop() >= 1000) {
+            $('.back-to-top').addClass('show');
+        }
+        else {
+            $('.back-to-top').removeClass('show');
+        }
+    });
+    // scroll to top
+    var toTop = $('.back-to-top');
+    // logic
+    toTop.on('click', function() {
+        $('html, body').animate({
+        scrollTop: $('html, body').offset().top,
+        },2000);
+    });
+
 });
